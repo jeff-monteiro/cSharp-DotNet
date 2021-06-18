248234390a4928c0cc4bf17f9875c5365db7dcfc
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-17 23:50:07 -0300

    URI_1010Update

--- URI_1010/Program.cs
+++ URI_1010/Program.cs
@@ -7,6 +7,8 @@ namespace URI_1010
         static void Main(string[] args)
         {
             Console.WriteLine("I need to do more exercises for to improve my habilities!");
+            Console.WriteLine("Karoliny Freitas");
+            Console.WriteLine("Jefferson Monteiro");
         }
     }
 }
