19536f03955c28ad215900a4be6cb717c931d5c3
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-17 23:46:04 -0300

    URI_1010

---
+++ URI_1010/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URI_1010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I need to do more exercises for to improve my habilities!");
+        }
+    }
+}
