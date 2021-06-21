5b34f07eb4f88fb53325d74773b172bce232ad4f
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-21 19:11:59 -0300

    Exercício_Condicionais_01

---
+++ Exercicio_Resolvido_01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Exercicio_Resolvido_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, soma;
+
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            soma = nota1 + nota2;
+
+            Console.WriteLine("Nota Final: " + soma.ToString("F1", CultureInfo.InvariantCulture));
+            if(soma < 60){
+                Console.WriteLine("REPROVADO!");
+            }else{
+                Console.WriteLine("APROVADO!");
+            }
+
+        }
+    }
+}
