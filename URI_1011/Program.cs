31912fc160d931506de1750cba22eb5889fd98a8
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-21 14:28:51 -0300

    URI_1011

--- URI_1011/Program.cs
+++ URI_1011/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace URI_1011
 {
@@ -6,7 +7,15 @@ namespace URI_1011
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //programa que clacula e mostra o volume de uma esfera.
+            double raio, volume;
+            
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            volume = 4.0 / 3 * 3.14159 * raio * raio * raio;
+
+            Console.WriteLine("Volume = " + volume.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
