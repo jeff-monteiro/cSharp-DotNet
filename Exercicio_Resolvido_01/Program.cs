7946eaa3a3e905b189e00f50c7d586992638455e
jeff-monteiro <jeffmonteiro61@outlook.com>
2021-06-21 19:13:31 -0300

    Condicionais_01

--- Exercicio_Resolvido_01/Program.cs
+++ Exercicio_Resolvido_01/Program.cs
@@ -16,7 +16,7 @@ namespace Exercicio_Resolvido_01
             soma = nota1 + nota2;
 
             Console.WriteLine("Nota Final: " + soma.ToString("F1", CultureInfo.InvariantCulture));
-            if(soma < 60){
+            if(soma < 60.0){
                 Console.WriteLine("REPROVADO!");
             }else{
                 Console.WriteLine("APROVADO!");
