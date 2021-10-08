3d455fc7a7a9f1eb67c366e291e403e31062cbcf
Jefferson <jeffmonteiro61@icloud.com>
2021-10-08 00:01:43 -0300

    comments_1036

--- URI_1036/Program.cs
+++ URI_1036/Program.cs
@@ -15,12 +15,14 @@ namespace URI_1036
             b = double.Parse(vet[1], CultureInfo.InvariantCulture);
             c = double.Parse(vet[2], CultureInfo.InvariantCulture);
 
+            //Math.Pow está elevando o valor de b 
             delta = Math.Pow(b, 2.0) - 4 * a * c;
 
             if(a == 0 || delta < 0.0){
                 Console.WriteLine("Impossível Calcular!");
             }
             else{
+                //Math.Sqrt está calculando a raíz quadrada do valor
                 r1 = (-b + Math.Sqrt(delta))/(2.0 * a);
                 r2 = (-b - Math.Sqrt(delta))/(2.0 * a);
                 Console.WriteLine("Resultado de R1 = "+ r1.ToString("F5", CultureInfo.InvariantCulture));
