a9b17fdf8ef01bcb3959fab7bf9ef704e55716d4
Jefferson <jeffmonteiro61@icloud.com>
2021-10-11 22:30:59 -0300

    Execercicio_Condicional

---
+++ Exercício_EstruturaCondicional_II/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercício_EstruturaCondicional_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int num1, num2, num3;
+
+           num1 = int.Parse(Console.ReadLine());
+           num2 = int.Parse(Console.ReadLine());
+           num3 = int.Parse(Console.ReadLine());
+
+           if(num1 < num2 && num1 < num3){
+               Console.WriteLine("O número: "+ num1 + " é o menor número!");
+           }
+
+
+        //    Console.WriteLine(num1);
+        //    Console.WriteLine(num2);
+        //    Console.WriteLine(num3);
+           
+
+           Console.ReadLine();
+        }
+    }
+}
