3e792049c5dd8e19786358727f4231995d88064e
Jefferson <jeffmonteiro61@icloud.com>
2021-10-11 22:48:16 -0300

    The-smallest-number-test

--- Exercício_EstruturaCondicional_II/Program.cs
+++ Exercício_EstruturaCondicional_II/Program.cs
@@ -8,13 +8,23 @@ namespace Exercício_EstruturaCondicional_II
         {
            int num1, num2, num3;
 
-           num1 = int.Parse(Console.ReadLine());
-           num2 = int.Parse(Console.ReadLine());
-           num3 = int.Parse(Console.ReadLine());
+           String[] vet = Console.ReadLine().Split(',');
+
+           num1 = int.Parse(vet[0]);
+           num2 = int.Parse(vet[1]);
+           num3 = int.Parse(vet[2]);
+
+           
 
            if(num1 < num2 && num1 < num3){
                Console.WriteLine("O número: "+ num1 + " é o menor número!");
+           }else if(num2 < num3){
+               Console.WriteLine("Menor: " + num2);
+           }else{
+               Console.WriteLine("Menor: " + num3);
            }
+               
+           
 
 
         //    Console.WriteLine(num1);
