753fe008c42f994c7225b890dbfb7c0668f9d0ae
Jefferson <jeffmonteiro61@icloud.com>
2021-11-30 22:13:37 -0300

    firstChallenge

---
+++ FirstChallengeDIO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstChallengeDIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] test = Console.ReadLine().Split(' ');
+            double X = double.Parse(test[0]);
+            double Y = double.Parse(test[1]);
+
+            
+        }
+    }
+}
