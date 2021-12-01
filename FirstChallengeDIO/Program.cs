9007ba2e7d09b12844efc805a6d6ee0e054e41aa
Jefferson <jeffmonteiro61@icloud.com>
2021-11-30 22:42:49 -0300

    firstChallenge

--- FirstChallengeDIO/Program.cs
+++ FirstChallengeDIO/Program.cs
@@ -6,11 +6,16 @@ namespace FirstChallengeDIO
     {
         static void Main(string[] args)
         {
-            string[] test = Console.ReadLine().Split(' ');
+            string[] test = Console.ReadLine().Split(" ");
             double X = double.Parse(test[0]);
             double Y = double.Parse(test[1]);
 
-            
+            if(Y == 0){
+                Console.WriteLine("divisão impossível");
+            }else{
+                double divisao = X / Y;
+                Console.WriteLine(divisao.ToString("F2"));
+            }
         }
     }
 }
