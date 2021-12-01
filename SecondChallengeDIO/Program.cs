a7c631809e74f37f6b4187a9906bfcec7f0fd39e
Jefferson <jeffmonteiro61@icloud.com>
2021-12-01 13:33:48 -0300

    SecondChallege

---
+++ SecondChallengeDIO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SecondChallengeDIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double X, Y, distancia;
+
+            X = double.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
