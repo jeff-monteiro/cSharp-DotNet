d2d28dd483cda7651085f0d3887a27054dbf8a5c
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2021-12-09 14:04:05 -0300

    calCellphone

---
+++ calcCellphone/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace calcCellphone{
+    class Program{
+        static void Main(string[] args){
+
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if(minutos > 100){
+                conta = conta + (minutos - 100) * 2.0;
+            }
+            Console.WriteLine("O valor a pagar em R$ " + conta.ToString("F2"));
+
+        }
+    }
+}
