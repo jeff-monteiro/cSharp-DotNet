6c4e7d4ac1752fc9004f3893d3d9096250aae9f8
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2021-12-11 17:10:34 -0300

    mediaAluno

---
+++ media_Aluno/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace media_Aluno{
+
+    class Program{
+        static void Main(string[] args){
+        
+            String nome;
+            double av1, av2, av3, mediaFinal;
+
+            Console.WriteLine("Insira seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Nota av1: ");
+            av1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nota av2: ");
+            av2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nota av3: ");
+            av3 = double.Parse(Console.ReadLine());
+
+            mediaFinal = (av1 + av2 + av3) / 3;
+
+            Console.WriteLine("A média final do aluno: " + nome + " foi " + mediaFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+            if(mediaFinal >= 6){
+                Console.WriteLine("Aluno: Aprovado");
+            }else{
+                Console.WriteLine("Aluno: Reprovado");
+            }
+        }
+    }   
+}
+
+
+
+        
+
