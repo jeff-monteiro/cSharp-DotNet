6e7dad43428b3bee9c84b4017a6de80279e68465
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2021-12-14 23:21:38 -0300

    switchSintaxe

--- switchCommand/Program.cs
+++ switchCommand/Program.cs
@@ -5,10 +5,11 @@ namespace curso {
     class Program {
         static void Main(string[] args){
            
+           Console.WriteLine("Insira o dia em que deseja fazer o agendamento.");
            int x = int.Parse(Console.ReadLine());
            string dia;
 
-           switch (x) {
+           switch (x) { //Sintáxe do comando SWITCH para estruturas encadeadas.
                case 1:
                     dia = "Domingo";
                     break;
@@ -17,10 +18,25 @@ namespace curso {
                     break;
                case 3:
                     dia = "Terça";
+                    break;
+               case 4:
+                    dia = "Quarta";
+                    break;
+               case 5:
+                    dia = "Quinta";
+                    break;
+               case 6:
+                    dia = "Quinta";
+                    break;
+               case 7:
+                    dia = "Quinta";
+                    break;
+               default:
+                    dia = "Dia não válido!";
                     break;     
 
            }
-           Console.WriteLine("The day of the week is: " + dia);
+           Console.WriteLine("The day of the week is: "+ dia);
            Console.ReadLine();
 
 
