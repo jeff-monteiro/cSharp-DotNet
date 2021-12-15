4ed755b5c3568971ab84f3228e31a5fe1c570867
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2021-12-14 22:52:16 -0300

    switchCase

--- switchCommand/Program.cs
+++ switchCommand/Program.cs
@@ -1,29 +1,24 @@
 ﻿using System;
 using System.Globalization;
 
-namespace switchCommand {
-    class Program{
+namespace curso {
+    class Program {
         static void Main(string[] args){
            
            int x = int.Parse(Console.ReadLine());
-           String dia;
+           string dia;
 
-           if(x == 1){
-               dia = "Sunday";
-           }else if(x == 2) {
-               dia = "Monday";
-           }else if(x == 3) {
-               dia = "Tuesday";
-           }else if(x == 4) {
-               dia = "Wednesday";
-           }else if(x == 5) {
-               dia = "Thursday";
-           }else if(x == 6) {
-               dia = "Friday";
-           }else if(x == 7) {
-               dia = "Saturday";
-           }else {
-               Console.WriteLine("This day is not valid");
+           switch (x) {
+               case 1:
+                    dia = "Domingo";
+                    break;
+               case 2:
+                    dia = "Segunda";
+                    break;
+               case 3:
+                    dia = "Terça";
+                    break;     
+
            }
            Console.WriteLine("The day of the week is: " + dia);
            Console.ReadLine();
